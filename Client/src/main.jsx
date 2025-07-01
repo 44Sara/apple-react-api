586fee6133d3a287b45cff4cc823975d8bd6e5e4
Jerusalem Hailu <hailus02@gmail.com>
2025-07-01 15:04:20 -0400

    fix

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "./commonResource/css/bootstrap.css";
+// import "./commonResource/css/bootstrap.css";
+// import "./commonResource/css/styles.css";
+// import "./commonResource/js/bootstrap.js";
+// import "./commonResource/js/custom.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./commonResource/css/styles.css";
-import "./commonResource/js/bootstrap.js";
-import "./commonResource/js/custom.js";
+import "bootstrap";
+import "./commonResource/js/custom"
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 createRoot(document.getElementById("root")).render(
